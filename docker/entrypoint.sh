ffb88ad2165145eb303deb66c7ca438a40d31356
Sacha Telgenhof <sacha.telgenhof@springernature.com>
2022-07-04 13:05:22 +0900

    Included step to ask for required TOTP.
    Signed-off-by: Sacha Telgenhof <sacha.telgenhof@springernature.com>

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 cp /etc/resolv.conf /etc/resolv.conf.bak
 umount /etc/resolv.conf
-echo "nameserver 8.8.8.8" > /etc/resolv.conf
+echo "nameserver 8.8.8.8" >/etc/resolv.conf
 
 sysctl net.ipv4.conf.all.forwarding=1
 
 /fix-firewall.sh &
+
 /start-traps.sh &
 
 service dnsmasq start
@@ -13,10 +14,11 @@ service dnsmasq start
 /opt/cisco/anyconnect/bin/vpnagentd
 
 if [ -f /response.txt ]; then
-  cat /response.txt | envsubst '$VPN_PASSWORD' | /opt/cisco/anyconnect/bin/vpn -s && \
-  unset VPN_PASSWORD && \
-  echo "Enjoy your VPN connection!" && \
-  tail -f /dev/null
+	cat /response.txt | envsubst '$VPN_PASSWORD' | envsubst '$TOTP' | /opt/cisco/anyconnect/bin/vpn -s &&
+		unset VPN_PASSWORD &&
+		unset TOTP &&
+		echo "Enjoy your VPN connection!" &&
+		tail -f /dev/null
 else
-  /opt/cisco/anyconnect/bin/vpn
+	/opt/cisco/anyconnect/bin/vpn
 fi
